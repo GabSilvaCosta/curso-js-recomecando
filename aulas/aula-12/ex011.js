15f27b8af0df0b9ac9f5895a354c17c4a366234c
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-11-02 20:17:20 -0300

    condicoes

--- aulas/aula-12/ex011.js
+++ aulas/aula-12/ex011.js
@@ -1,6 +1,7 @@
-var idade = 66
+var idade = 70
+console.log(`Você tem ${idade} anos.`)
 if (idade < 16) {
-    console.log(`Você tem ${idade} anos.`)
+    console.log('Não vota.')
 }   else if (idade < 18 || idade > 65) {
         console.log('Voto opcional.')
 } else if (idade >= 18) {
