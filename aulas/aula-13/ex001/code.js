31f5aa831de28442a8eb43bd900b014ee84df9f7
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-11-03 10:42:34 -0300

    finalizando-ex_hora_atual

--- aulas/aula-13/ex001/code.js
+++ aulas/aula-13/ex001/code.js
@@ -6,12 +6,15 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //Bom dia!
-        img.src = 'fotomanha.jpg'
+        img.src = 'imagens/fotomanha.png'
+        document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
         //Boa tarde!
-        img.src = 'fototarde.jpg'
+        img.src = 'imagens/fototarde.png'
+        document.body.style.background = '#b9846f'
     } else {
         //Boa noite!
-        img.src = 'fotonoite.jpg'
+        img.src = 'imagens/fotonoite.png'
+        document.body.style.background = '#515154'
     }
 }
