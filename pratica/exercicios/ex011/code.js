bba1de0afbda166aafaf55f2b877ea009a15a129
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-11-04 13:48:42 -0300

    criando-validador-de-idade

---
+++ pratica/exercicios/ex011/code.js
@@ -0,0 +1,6 @@
+function clicou() {
+    var nome = String(document.getElementById('inome'))
+    var nomee = nome
+    var exibir = document.querySelector('div')
+    exibir.innerHTML = `${nomee}`
+}
