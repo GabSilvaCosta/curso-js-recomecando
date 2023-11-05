1f1bb8950e41c7f55bf94d9da0600b075e58cafc
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-11-05 11:24:54 -0300

    criando-imagens-ex013-ex002

--- pratica/exercicios/ex011/code.js
+++ pratica/exercicios/ex011/code.js
@@ -1,6 +1,10 @@
 function clicou() {
-    var nome = String(document.getElementById('inome'))
-    var nomee = nome
-    var exibir = document.querySelector('div')
-    exibir.innerHTML = `${nomee}`
+    var name = document.getElementById('inome').value
+    var age = Number(document.getElementById('iidade').value)
+    var conteiner = document.querySelector('div')
+    if (age >= 18) {
+        conteiner.innerHTML = `Olá ${name}, verifiquei que você é maior de idade.`
+    }   else {
+        conteiner.innerHTML = `Olá ${name}, verifiquei que você é menor de idade.`
+    }
 }
