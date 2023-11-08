514b4891e5cb940eeb08c81200397853103843bb
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-11-07 23:01:14 -0300

    create_simple_calculator

---
+++ pratica/exercicios/ex016/code.js
@@ -0,0 +1,7 @@
+function clicou() {
+    var v1 = Number(document.getElementById('ivalor1').value)
+    var v2 = Number(document.getElementById('ivalor2').value)
+
+    var dis = document.querySelector('div')
+    dis.innerHTML = 'r'
+}
