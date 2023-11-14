654fa4a14aa9a8bd88ddc48299a41dbca65301da
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-11-14 09:34:04 -0300

    Create-ex018

---
+++ pratica/exercicios/ex018/code.js
@@ -0,0 +1,5 @@
+function msg() {
+    var nome = window.prompt('Qual é o seu nome?')
+    window.alert(`Seja bem vindo ${nome}`)
+}
+// A function acima pergunta ao user o nome dele e após isto exibe um alert na tela, o mesmo contem uma mensagem de boas vindas ao user!
