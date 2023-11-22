9c9296879b101883a14e0591bc98f6d28e038687
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-11-22 10:02:12 -0300

    verificar-par-impar

--- pratica/exercicios/ex022/code.js
+++ pratica/exercicios/ex022/code.js
@@ -1,5 +1,10 @@
-let button = document.querySelector('#verificadorBtn')
-let mostrar = document.querySelector('p')
+let button = document.querySelector('#verificadorBtn') // Liga o input:button com o JS.
+let mostrar = document.querySelector('#exibir')
 button.addEventListener('click', function(){
-    
+    let valor = Number(document.querySelector('#ivalue').value)
+    if (valor % 2 === 0) {
+        mostrar.innerHTML = `O número ${valor} é par.`
+    } else {
+        mostrar.innerHTML = ` O número ${valor} é impár.`
+    }
 })
