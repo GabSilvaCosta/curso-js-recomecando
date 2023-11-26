cce7c95276b29b19e5d04ba8eac26eec4eccb880
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-11-26 14:41:59 -0300

    Work-project-calculator

---
+++ pratica/exercicios/ex023/code.js
@@ -0,0 +1,74 @@
+let tela = document.getElementById('ienter-data')
+
+function sete() {
+    var sete = 7
+    tela.value += sete
+}
+
+function nove() {
+    var nove = 9
+    tela.value += nove
+}
+
+function oito() {
+    var oito = 8
+    tela.value += oito
+}
+
+function mult() {
+    var multiplicacao = 'X'
+    tela.value += multiplicacao
+}
+
+function quatro() {
+    var quatro = 4
+    tela.value += quatro
+}
+
+function cinco() {
+    var cinco = 5
+    tela.value += cinco
+}
+
+function seis() {
+    var seis = 6
+    tela.value += seis
+}
+
+function sub() {
+    var subtracao = '-'
+    tela.value += subtracao
+}
+
+function um() {
+    var um = 1
+    tela.value += um
+}
+
+function dois() {
+    var dois = 2
+    tela.value += dois
+} 
+
+function tres() {
+    var tres = 3
+    tela.value += tres
+}
+
+function adi() {
+    var adicao = '+'
+    tela.value += adicao
+}
+
+function zero() {
+    var zero = 0
+    tela.value += zero
+}
+
+function apagar() {
+    var del = tela.value = ''
+}
+
+function realizarop() {
+    
+}
