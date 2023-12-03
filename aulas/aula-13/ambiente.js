faee2acf6b4c258f2f422b634bc5de876c88f1a2
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-12-03 10:26:18 -0300

    rrelembrando-estruturas-de-repetição

--- aulas/aula-13/ambiente.js
+++ aulas/aula-13/ambiente.js
@@ -1,13 +1,13 @@
-var c = 1 
+var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
-} while (c <= 5)
+} while (c <= 6)
 
-// var c = 1 
-// while (c <= 5) {
-//     console.log(`Passo ${c}`)
-//     c++
-// }
-
-// console.log('Tudo bem?')
+/*
+var c = 1;
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
---
+++ pratica/exercicios/ex024/code.js
@@ -0,0 +1,6 @@
+let iNumber = Number(window.prompt('What inicial number?'))
+let result = document.querySelector('p')
+while (iNumber <= 0) {
+    result.innerHTML = "teste"
+    iNumber++
+}
