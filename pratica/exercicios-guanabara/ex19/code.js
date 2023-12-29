3bf171a9f3731f397512a00968ee1dbbe9a91021
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-12-29 08:35:19 -0300

    resolvendo-EXs-logica-programacao

--- pratica/exercicios-guanabara/ex19/code.js
+++ pratica/exercicios-guanabara/ex19/code.js
@@ -1,10 +1,16 @@
 let exib = document.querySelector('p')
 let button = document.querySelector('button').addEventListener('click', function() {
     let mesUser = window.prompt('Digite o mês em extenso (ex: Setembro)')
-    if (mesUser == 'Janeiro') {
+    if (mesUser == 'Janeiro' || mesUser == 'Fevereiro' || mesUser == 'Março') {
         exib.innerHTML = `No mês de ${mesUser}, estamos na estação <strong>INVERNO</strong>.`
         exib.style.color = 'green'
-    } else if (mesUser == 'Fevereiro') {
-        exib.innerHTML = `No mês de ${mesUser}, estamos na estação <strong>INVERNO</strong>.`
+    } else if (mesUser == 'Abril' || mesUser == 'Maio' || mesUser == 'Junho') {
+        exib.innerHTML = `No mês de ${mesUser}, estamos na estação <strong>PRIMAVERA</strong>.`
+    } else if (mesUser == 'Julho' || mesUser == 'Agosto' || mesUser == 'Setembro') {
+        exib.innerHTML = `No mês de ${mesUser}, estamos na estação <strong>VERÃO</strong>.`
+    } else if (mesUser == 'outubro' || mesUser == 'Novembro' || mesUser == 'Dezembro') {
+        exib.innerHTML = `No mês de ${mesUser}, estamos na estação <strong>OUTONO</strong>.`
+    } else {
+        exib.innerHTML = `No mês ${mesUser}, estamos na estação <strong>INDEFINIDA</strong>`
     }
 })
