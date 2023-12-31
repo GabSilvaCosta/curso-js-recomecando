feb595b7871f8d821e409446af0e1cd47c9f300b
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2023-12-31 16:29:38 -0300

    Resolvenso exercícios-JS

---
+++ pratica/exercicios-guanabara/ex21/code.js
@@ -0,0 +1,16 @@
+let result = document.querySelector('#container')
+let botao = document.querySelector('button').addEventListener('click', function() {
+    result.innerHTML += `<br><br>Contando de 1 até 10<br>`
+    result.style.fontFamily = 'Single Day'
+    let v = 0
+        while (v < 10) {
+            v++
+            // result.innerHTML += `&#x1F449 ${v}`
+            if (v % 2 == 0) {
+                result.innerHTML += `&#x1F449 <strong>${v}</strong>`
+            } else {
+                result.innerHTML += `&#x1F449 ${v}`
+            }
+        }
+    result.innerHTML += `&#x1F449&#x1F3C1`
+})
