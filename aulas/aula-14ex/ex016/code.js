1d59cb5f7dfc7322d4835f0f075e1bbb9ddf18c1
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-01-27 09:22:37 -0300

    praticando_JS

--- aulas/aula-14ex/ex016/code.js
+++ aulas/aula-14ex/ex016/code.js
@@ -1,33 +1,4 @@
-let botao = document.querySelector('#botao');
-    botao.addEventListener('click', function() {
-        let ini = document.getElementById('inicio')
-        let fim = document.getElementById('fim')
-        let passo = document.getElementById('passo')
-        let res = document.getElementById('res')
-        // As linhas de 8 a 11 servem para verificar se o user inseriu algum valor em todos os inputs.
-        if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-            res.innerHTML = "Impossível contar!"
-            window.alert('[Erro] Faltam dados!')
-        } else {
-            let i = Number(ini.value)
-            let f = Number(fim.value)
-            let p = Number(passo.value)
-            if ( p <= 0) {
-                window.alert('Passo invalido! Considerando passo 1')
-                p = 1
-            }
-            if (i < f) {
-                // Contagem crescente
-                for (let c = i; c <= f; c += p) {
-                    res.innerHTML += ` ${c} \u{1F449}`
-                }
-            } else {
-                // Contagem regressiva
-                for (let c = i; c >= f; c -= p) {
-                    res.innerHTML += `${c} \u{1F449}`
-                }
-            }
-            res.innerHTML += `\u{1F3C1}`
-        }
-    })
-    
+ let exib = document.querySelector('#res')
+ let Button = document.querySelector('#botao').addEventListener('click', funcition() {
+    let v_inicio = document.querySelector('#inicio')
+ })
--- pratica/exercicios-guanabara/ex026/code.js
+++ pratica/exercicios-guanabara/ex026/code.js
@@ -1,6 +1,12 @@
-let Botao = document.querySelector('button').addEventListener('click', function() {
-    let onde_aparece = document.querySelector('div')
-    let v_user = document.querySelector('#valor').value
-    let V_user = Number(v_user)
-    
+let exib = document.querySelector('div')
+let botao = document.querySelector('button').addEventListener('click', function() {
+    let valor = document.querySelector('#valor').value
+    let valor_trat = Number(valor)
+    let contador = 1
+    exib.innerHTML = `<h2>Tabuada do ${valor_trat}</h2>`
+    while (contador <= 10) {
+        let resultado = valor_trat * contador
+        exib.innerHTML += `${valor_trat} X ${contador} = <strong>${resultado}</strong><br>`
+        contador++
+    }
 })
