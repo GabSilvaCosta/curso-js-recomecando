36b901157b254f4af1de96c145f151a3355059b5
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-01-27 21:10:12 -0300

    Update code.js

--- aulas/aula-14ex/ex016/code.js
+++ aulas/aula-14ex/ex016/code.js
@@ -1,4 +1,16 @@
  let exib = document.querySelector('#res')
- let Button = document.querySelector('#botao').addEventListener('click', funcition() {
-    let v_inicio = document.querySelector('#inicio')
+ let Button = document.querySelector('#botao').addEventListener('click', function() {
+    let v_inicio = document.querySelector('#inicio').value
+    let V_inicio = Number(v_inicio)
+
+    let v_final = document.querySelector('#fim').value
+    let V_final = Number(v_final)
+
+    let step_ini = document.querySelector('#passo').value
+    let Step_ini = Number(step_ini)
+
+    while (V_inicio <= V_final) {
+      exib.innerHTML += `${V_inicio}`
+      V_inicio++
+    }
  })
