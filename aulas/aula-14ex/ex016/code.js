1d1bb8503dfdecc4978162170700101cd66a6b5c
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-01-31 16:57:47 -0300

    finalizando-ex16-comecando-ex17

--- aulas/aula-14ex/ex016/code.js
+++ aulas/aula-14ex/ex016/code.js
@@ -8,9 +8,23 @@
 
     let step_ini = document.querySelector('#passo').value
     let Step_ini = Number(step_ini)
-    
+    if (V_inicio == '') {
+      exib.innerHTML = `Impossível contar!`
+    } else if (Step_ini == ``) {
+      window.alert('PASSO inválido! Considerando PASSO 1')
+      exib.innerHTML += `<br>`
     while (V_inicio <= V_final) {
-      exib.innerHTML += `${V_inicio}`
+      exib.innerHTML += `${V_inicio} &#x1F449`
       V_inicio++
+    }
+    exib.innerHTML += `&#x1F3C1`
+    }
+    else {
+      exib.innerHTML += `<br>`
+    while (V_inicio <= V_final) {
+      exib.innerHTML += `${V_inicio} &#x1F449`
+      V_inicio = V_inicio + Step_ini 
+    }
+    exib.innerHTML += `&#x1F3C1`
     }
  })
