3e87a4388c095460e7ac6cf70d8576013dd545b5
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-02-11 16:34:23 -0300

    learning of arrays

--- aulas/aula-15/ambiente.js
+++ aulas/aula-15/ambiente.js
@@ -1,4 +1,12 @@
-let num = [2, 5, 6]
-num[4] = 4
-
-console.log(`Nosso vetor é o ${num}`)
+let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é `)
+let pos = num.indexOf(10)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
