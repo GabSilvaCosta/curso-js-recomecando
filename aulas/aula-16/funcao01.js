ebe6531e509858e1af4bf22cdbdaf996990227fe
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-02-13 16:50:12 -0300

    aprendendo-sobre-functions

---
+++ aulas/aula-16/funcao01.js
@@ -0,0 +1,17 @@
+function parImpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+/*
+Método 1 para exibir o valor da function
+let res = parImpar(4)
+console.log(res)
+*/
+
+/*
+Método 2 para exibir o valor da function
+console.log(parImpar(4))
+*/
