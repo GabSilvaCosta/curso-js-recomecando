f010495fa84763c2cf4c40f4c8a897b20b3e144d
Gabriel Costa <gaab28082003@gmail.com>
2024-03-10 14:04:34 -0300

    Delete node_490e41d45ad8f.tmp.js

--- aulas/aula-11/node_490e41d45ad8f.tmp.js
+++
@@ -1,6 +0,0 @@
-var vel = 10
-console.log(`A velocidade do seu carro é ${vel}Km/h por hora!`)
-if (vel > 60) { //Condição simples!
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
-}
-console.log('Dírija sempre usando cinto de segurança')
