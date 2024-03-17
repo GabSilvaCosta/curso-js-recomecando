416843f1dd6370f5f4f0bc3fcfc0efcb5b723198
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-03-17 07:42:05 -0300

    estudando-no-domingo

---
+++ aulas/aula-14/aula-14ex/ex004-002/code.js
@@ -0,0 +1,5 @@
+let button = document.querySelector('#botao').addEventListener('click', function() {
+    let Fvalue = document.querySelector('#Vinicial')
+    let Lvalue = document.querySelector('#VFinal')
+    let Steps = document.querySelector('#Idstep')
+})
