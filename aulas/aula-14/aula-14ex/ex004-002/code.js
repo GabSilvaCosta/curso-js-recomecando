50c955cf15f2a671999bb5b93b6928080b75507b
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-03-17 09:01:52 -0300

    finalizando-contador-de-numeros
    Criei um contator de números

--- aulas/aula-14/aula-14ex/ex004-002/code.js
+++ aulas/aula-14/aula-14ex/ex004-002/code.js
@@ -2,4 +2,26 @@ let button = document.querySelector('#botao').addEventListener('click', function
     let Fvalue = document.querySelector('#Vinicial')
     let Lvalue = document.querySelector('#VFinal')
     let Steps = document.querySelector('#Idstep')
+    let res = document.querySelector('#res')
+    if (Fvalue.value.length == 0) {
+        res.innerHTML = `Impossível contar!`
+    } else if (Number(Steps.value) == 0) {
+        window.alert('Passo inválido, considerando PASSO 1.')
+        res.innerHTML = 'Contando:<br>'
+        for ( let c = Number(Fvalue.value); c <= Number(Lvalue.value); c++) {
+            res.innerHTML += `${c} &#x1F449`
+        }
+        res.innerHTML += `&#x1F3C1`
+    } else {
+        res.innerHTML = 'Contando:<br>'
+        for (let c = Number(Fvalue.value); c <= Number(Lvalue.value); c = c + Number(Steps.value)) {
+            res.innerHTML += `${c} &#x1F449`
+        }
+        res.innerHTML += `&#x1F3C1`
+    }
 })
+
+/* 
+&#x1F449
+&#x1F3C1
+*/
