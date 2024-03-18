3afa4abb4cd81c79dfc7758857c1bc937b16279d
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-03-18 16:41:52 -0300

    Update code.js

--- aulas/aula-14/aula-14ex/ex004-002/code.js
+++ aulas/aula-14/aula-14ex/ex004-002/code.js
@@ -3,25 +3,32 @@ let button = document.querySelector('#botao').addEventListener('click', function
     let Lvalue = document.querySelector('#VFinal')
     let Steps = document.querySelector('#Idstep')
     let res = document.querySelector('#res')
-    if (Fvalue.value.length == 0) {
+    if (Fvalue.value.length == 0 || Lvalue.value.length == 0) {
         res.innerHTML = `Impossível contar!`
-    } else if (Number(Steps.value) == 0) {
+    } else if (Number(Fvalue.value) < Number(Lvalue.value) && Number(Steps.value) == 0) {
         window.alert('Passo inválido, considerando PASSO 1.')
         res.innerHTML = 'Contando:<br>'
         for ( let c = Number(Fvalue.value); c <= Number(Lvalue.value); c++) {
             res.innerHTML += `${c} &#x1F449`
         }
         res.innerHTML += `&#x1F3C1`
-    } else {
+    } else if (Number(Fvalue.value) > Number(Lvalue.value) && Number(Steps.value) == 0) {
+        window.alert('Passo inválido, considerando PASSO 1.')
+        res.innerHTML = 'Contando:<br>'
+        for ( let c = Number(Fvalue.value); c >= Number(Lvalue.value); c--) {
+            res.innerHTML += `${c} &#x1F449`
+        }
+    } else if (Fvalue.value < Lvalue.value) {
         res.innerHTML = 'Contando:<br>'
         for (let c = Number(Fvalue.value); c <= Number(Lvalue.value); c = c + Number(Steps.value)) {
             res.innerHTML += `${c} &#x1F449`
         }
         res.innerHTML += `&#x1F3C1`
+    } else if (Fvalue.value > Lvalue.value) {
+        res.innerHTML = 'Contando:<br>'
+        for (let c = Number(Fvalue.value); c >= Number(Lvalue.value); c = c - Number(Steps.value)) {
+            res.innerHTML += `${c} &#x1F449`
+        }
+        res.innerHTML += `&#x1F3C1`
     }
 })
-
-/* 
-&#x1F449
-&#x1F3C1
-*/
