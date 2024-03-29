5c0539f8c468d6490f657f8e7ff1261d10d4198f
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-03-29 16:00:00 -0300

    recapitulando-arrays-e-functions
    Estou tendo um pouco de dificuldades com o uso de arrays e também com o uso de parâmetros dentro de functions, porém tenho certesa de que com muita prática conseguirei ficar bom.

--- aulas/aula-15/ambiente001.js
+++ aulas/aula-15/ambiente001.js
@@ -1,3 +1,5 @@
+/*
+Primeira tentativa de entender array
 let num = [5, 8, 2, 9, 3]
 num.push(1)
 num.sort()
@@ -10,3 +12,11 @@ if (pos == -1) {
 } else {
     console.log(`O valor 8 está na posição ${pos}`)
 }
+*/
+// Segunda tentativa de entender array.
+let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
--- aulas/aula-15/ambiente002.js
+++ aulas/aula-15/ambiente002.js
@@ -1,3 +1,5 @@
+/*
+Primeira tentativa de aprender array
 let num = [5, 8, 2, 9, 3]
 num.push(1)
 num.sort()
@@ -9,4 +11,13 @@ if (pos == -1) {
     console.log('O valor não foi encontrado!')
 } else {
     console.log(`O valor 8 está na posição ${pos}`)
+}
+*/
+// Segunda tentativa de aprender array
+let num = [5, 8, 2, 9, 3]
+let pos = num.indexOf(18)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
 }
