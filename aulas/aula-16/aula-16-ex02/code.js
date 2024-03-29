99988e6f8c7c39ff552af552844368aa2fb25727
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-03-29 17:09:27 -0300

    Update code.js

--- aulas/aula-16/aula-16-ex02/code.js
+++
@@ -1,8 +0,0 @@
-let userV = document.querySelector('#Uvalor')
-let res = document.querySelector('#res')
-let lista = document.querySelector('#valuelist')
-let valores = []
-
-function adicionar() {
-    
-}
