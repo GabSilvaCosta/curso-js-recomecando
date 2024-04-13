8d90a0f98827fd6a2e5b68801d013b4aaa9284c6
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-04-13 14:44:06 -0300

    Finish-exercise-simple-clock

--- aulas/aula-12/ex001-12-4/code.js
+++ aulas/aula-12/ex001-12-4/code.js
@@ -1,13 +1,17 @@
 let time = new Date()
 let hours = time.getHours()
-let res = document.querySelector('#resTime')
+let res = document.querySelector('#resTime').innerHTML = `Now are ${hours}:00 o´clock`
 let image = document.querySelector('img')
 let body = document.querySelector('body')
 document.addEventListener('DOMContentLoaded', function() {
     if (hours < 12) {
         body.style.background = 'burlywood'
-    } else if (hours < 18) {
+        image.src = 'image/fotomanha.png'
+    } else if (hours < 19) {
         body.style.background = 'rgb(133, 133, 133)'
         image.src = 'image/fototarde.png'
+    } else {
+        body.style.background = 'rgb(49, 70, 94)'
+        image.src = 'image/fotonoite.png'
     }
 })
