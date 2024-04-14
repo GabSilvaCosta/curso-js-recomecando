458621ac6c176e4153505f474bfc26ae71057e84
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-04-14 15:47:11 -0300

    Smart-caunt
    I make a smart caunt.

---
+++ aulas/aula-14/aula-14ex/ex004-003/code.js
@@ -0,0 +1,11 @@
+let inputStart = document.querySelector('#StartUser')
+let inputEnd = document.querySelector('#EndUser')
+let inputStep = document.querySelector('#StepUser')
+let res = document.querySelector('#res')
+let button = document.querySelector('#Ibutton').addEventListener('click', function() {
+    if (Number(inputStart.value == 0 || Number(inputEnd.value) == 0) {
+        window.alert('[ERROR] A box  ')
+    } else if () {
+
+    }
+})
