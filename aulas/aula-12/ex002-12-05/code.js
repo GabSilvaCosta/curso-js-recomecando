592909e86c65c34653973b880b2dabf0766cf47f
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-04-14 11:21:23 -0300

    finish-age-verify

--- aulas/aula-12/ex002-12-05/code.js
+++ aulas/aula-12/ex002-12-05/code.js
@@ -1,14 +1,47 @@
 let data = new Date()
 let CurrentYear = data.getFullYear()
 let bornUser = document.querySelector('#bornUser')
+let res = document.querySelector('#res')
 let button = document.querySelector('#Iverify').addEventListener('click', function() {
     if (Number(bornUser.value) > CurrentYear || Number(bornUser.value) == 0) {
-        window.alert('[ERROR] The value entered is greater than the current year.')
+        window.alert('[ERROR] The value entered is equal the 0 or greater than the current year.')
     } else {
+        let WomanOrMan = document.getElementsByName('sexUser')
+        let gender = ''
+        let age = CurrentYear - Number(bornUser.value)
         let img = document.createElement('img')
         img.setAttribute('id', 'people')
-        let age = CurrentYear - Number(bornUser.value)
-        res.innerHTML = `The age of user is ${age}`
-    }
+        if ( WomanOrMan[0].checked) {
+            gender = 'man'
+                if (age <= 11) {
+                    res.innerHTML = `We detected a child ${gender} with ${age} year/years.`
+                    img.src = 'images/baby-boy-circle.png'
+                    } else if (age <= 21) {
+                        res.innerHTML = `We detected a young ${gender} with ${age} years.`
+                        img.src = 'images/young-boy-circle.png'
+                    } else if (age <= 50) {
+                        res.innerHTML = `We detected a ${gender} with ${age} years.`
+                        img.src = 'images/adult-man-circle.png'
+                    } else {
+                        res.innerHTML = `We detected a old ${gender} with ${age} years.`
+                        img.src = 'images/old-man-circle.png'
+                    }
+                } else {
+                    gender = 'woman'
+                    if ( age <= 11) {
+                        res.innerHTML = `We detected a child ${gender} with ${age} year/years.`
+                        img.src = 'images/baby-girl-circle.png'
+                    } else if (age <= 21) {
+                        res.innerHTML = `We detected a young ${gender} with ${age} years.`
+                        img.src = 'images/young-girl-circle.png'
+                    } else if (age <= 50) {
+                        res.innerHTML = `We detected a ${gender} with ${age} years.`
+                        img.src = 'images/adult-woman-circle.png'
+                    } else {
+                        res.innerHTML = `We detected a  old ${gender} with ${age} years.`
+                        img.src = 'images/old-woman-circle.png'
+                    }
+                }
+                res.appendChild(img)
+            }
 })
-let res = document.querySelector('#res')
