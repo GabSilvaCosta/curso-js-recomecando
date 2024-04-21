1a095d41aa26b03acc31238b856597b1555a5a15
GabSilvaCosta <132613612+GabSilvaCosta@users.noreply.github.com>
2024-04-21 17:21:32 -0300

    study-programing-JS

---
+++ curso-mdm/05-array/learning-about-array.js
@@ -0,0 +1,9 @@
+let shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles']
+
+let sequence = [1, 1, 2, 3, 5, 8, 13]
+
+let random = ['tree', 795, [0, 1, 2]]
+
+let GabsData = ['Brazilian', 20, 70,  'student']
+shopping[0] = 'tahini'
+console.log(sequence.length)
---
+++ curso-mdm/06-gerador_historias_bobas/main.js
@@ -0,0 +1,5 @@
+let Uname = document.querySelector('#customname')
+let res = document.querySelector('.story')
+let Bgenerate = document.querySelector('.randomize').addEventListener('click', function() {
+    
+})
